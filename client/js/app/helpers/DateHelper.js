7dc05b831ecf7f68c257a6e9f7d9099b9433d664
Leonardo Maier <leonarmaier@hotmail.com>
2017-05-22 22:36:06 -0300

    Lista de Negociações adicionada

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -1,15 +1,20 @@
 class DateHelper {
 
-	constructor() {}
+	// Como os métodos são static, não é necessário instanciar, gerando assim um erro
+	constructor() {
+		throw new Error('Esta classe não pode ser instanciada');
+	}
 
 	static dateToString(date) {
-		return 	date.getDate() 
-			+ '/' + (date.getMonth() + 1)
-			+ '/' + date.getFullYear();
+		return `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
 	}
 
 
 	static textToDate(text) {
+		
+		if(!/\d{4}-\d{2}-\d{2}/.test(text)) 
+			throw new Error('Data informada no formato inválido');
+
 		return new Date(...text.split('-').map((item, index) => item - index % 2));
 	}
 }
