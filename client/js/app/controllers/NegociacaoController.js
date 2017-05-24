3a745a4daace419e11fc64d4007d3bc9752f8581
Leonardo Maier <leonarmaier@hotmail.com>
2017-05-24 11:32:43 -0300

    Added ProxyFactory and Proxy pattern

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -8,25 +8,22 @@ class NegociacaoController {
 		this._inputQuantidade = $('#quantidade');
 		this._inputValor = $('#valor');
 
-		/*this._listaNegociacoes = new ListaNegociacoes(model => 
-			this._negociacoesView.update(model));*/
+		this._listaNegociacoes = new Bind(
+			new ListaNegociacoes(),
+			new NegociacoesView($('#negociacoesView')),
+			'adiciona', 'esvazia');
 
-		this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-		this._negociacoesView.update(this._listaNegociacoes);
-
-		this._mensagem = new Mensagem();
-		this._mensagemView = new MensagemView($('#mensagemView'));
-		this._mensagemView.update(this._mensagem);
+		this._mensagem = new Bind(
+			new Mensagem(),
+			new MensagemView($('#mensagemView')),
+			'texto');
 	}
 
 	adiciona(event) {
 		
 		event.preventDefault();
 		this._listaNegociacoes.adiciona(this._criaNegociacao());
-	
 		this._mensagem.texto = 'Negociação adicionada com sucesso!';
-		this._mensagemView.update(this._mensagem);
-		
 		this._limpaFormulario();
 
 	}
@@ -34,9 +31,7 @@ class NegociacaoController {
 	apaga() {
 
 		this._listaNegociacoes.esvazia();
-
 		this._mensagem.texto = 'Negociações apagadas com sucesso!';
-		this._mensagemView.update(this._mensagem);
 	}
 
 	_criaNegociacao() {
